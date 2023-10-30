6249bca6a591102e0130e9a59eb6c4085d665d82
Vinicius Henrique <vile.pb@gmail.com>
2023-10-30 00:40:00 -0300

    configuring logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/vinisioux/go-api/router"
+import (
+	"github.com/vinisioux/go-api/config"
+	"github.com/vinisioux/go-api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
 	router.Init()
 }
